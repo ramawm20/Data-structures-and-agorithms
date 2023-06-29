ed4658f501651668583afe31ff2e1d1ca308eb57
ramawm20 <ramawael54@gmail.com>
2023-06-29 16:11:36 +0300

    Code Challenge 05 has been solved

--- LinkedList/Program.cs
+++ LinkedList/Program.cs
@@ -1,10 +1,86 @@
 ﻿namespace LinkedList
 {
-    internal class Program
+    public  class Program
     {
+        public class Node
+        {
+            public int value { get; set; }
+            public Node nextNode { get; set; }
+
+            public Node(int value)
+            {
+                this.value = value;
+                nextNode = null;
+            }
+        }
+        public class LinkedList
+        {
+            public Node head;
+
+            public LinkedList()
+            {
+                head = null;
+            }
+
+            public void Insert(int value)
+            {
+                Node newNode = new Node(value);
+                newNode.nextNode = head;
+                head = newNode;
+            }
+
+            public bool Includes(int value)
+            {
+                Node currentNode= head;
+                while (currentNode != null)
+                {
+                    if (currentNode.value == value)
+                    {
+                        return true;
+                    }
+                    currentNode = currentNode.nextNode;
+                   
+                }
+                return false;
+            }
+            public string toString()
+            {
+                if (head == null)
+                {
+                    return "NULL";
+                }
+
+                Node currentNode = head;
+                string res = "";
+                while (currentNode != null)
+                {
+                    res += ($"{{{currentNode.value}}} -> ");
+                    currentNode = currentNode.nextNode;
+                }
+                res += "NULL";
+                return res;
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            LinkedList newList=new LinkedList();
+            newList.Insert(30);
+            newList.Insert(40);
+            newList.Insert(50);
+            newList.Insert(60);
+
+            int value=newList.head.value;
+            Console.WriteLine(value );
+            
+            bool result;
+            result=newList.Includes(30);
+            Console.WriteLine(result);
+
+            string res;
+            res=newList.toString();
+            Console.WriteLine(res);
+
         }
     }
 }
---
+++ LinkedList/README.md
@@ -0,0 +1,17 @@
+# Challenge Title
+## Linked List
++ I created a simple singly Linked list and it's contain some features like the ability of insert a new node to the list, Search for a value to know if it included in the list or not and a it contains a method (toString()) that returns the values in the linked list
+
+## Whiteboard Process
+<!-- Embedded whiteboard image -->
+
+## Approach & Efficiency
++ First I created a class **Node** Which Initialize a value and the nextNode in the list 
++ Then the class **Linkedlist** which contains head that has the value null to ensure each time you create a linked list it's created an empty one.This class contains also a lot of methods  
++ 1. **Insert() method** It takes the value as an argument and returns nothing this method create new node with the given value, makes it points to the head then it changes the head to take the value of the new node.
+	2. **Includes() Method** This method takes a value and checks if it exist in the linked list or not First it initialized a current node to point at the head and checks the nodes untill it's null if the value matches the values at the list returns true else retirns false and after each check it changed the current to points at the next node.
+	1. **toString() Method** It returns the values in the linked list in specific format
+
+
+## Solution
+<!-- Show how to run your code, and examples of it in action -->
