b27360a7458dc6b971eedeab04a0511c0284531c
ramawm20 <ramawael54@gmail.com>
2023-07-30 01:39:48 +0300

    CC 15

---
+++ LinkedList/BinarySearchTree.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static LinkedList.Program;
+
+namespace LinkedList
+{
+    public class BinarySearchTree<T> : BinaryTree<T> where T : IComparable<T>
+    {
+        public void Add(T value)
+        {
+            Root = AddNode(Root, value);
+        }
+        private NodeTrees<T> AddNode(NodeTrees<T> node, T value)
+        {
+            if (node == null)
+                return new NodeTrees<T>(value);
+
+
+            if (value.CompareTo(node.value) < 0)
+                node.leftNode = AddNode(node.leftNode, value);
+
+            else if (value.CompareTo(node.value) > 0)
+                node.rightNode = AddNode(node.rightNode, value);
+
+            return node;
+        }
+        public bool Contains(int value)
+        {
+            return ContainsNode(Root, value);
+        }
+
+        private bool ContainsNode(NodeTrees<T> node, int value)
+        {
+            if (node == null)
+            {
+                return false;
+            }
+
+            if (node.value == value)
+            {
+                return true;
+            }
+            else if (value < node.value)
+            {
+                return ContainsNode(node.leftNode, value);
+            }
+            else
+            {
+                return ContainsNode(node.rightNode, value);
+            }
+        }
+    }
+}
