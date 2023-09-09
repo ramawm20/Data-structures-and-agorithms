b9a8acc8da7df9a633cf4bbdb4c79711c994a54f
ramawm20 <ramawael54@gmail.com>
2023-09-09 23:57:50 +0300

    Code challenge 28 has been solved

---
+++ Challenges/C#-Comparisions/Comparisons/Comparisons/Movie.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comparisons
+{
+	public class Movie
+	{
+		public string Title {  get; set; }
+
+		public int Year { get; set; }
+
+		public List<string> Genres { get; set; }
+	}
+
+}
